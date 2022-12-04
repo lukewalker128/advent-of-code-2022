83ac9d7d5f4f553306e9c333c1e4b751d2407dd1
Luke Walker <luke.walker.122@gmail.com>
2022-12-04 10:53:24 -0500

    Completed Day 1 challenge

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,25 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testCalories = testInput.toCalories()
+    check(testCalories.max() == 24_000)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val calories = input.toCalories()
+    println(calories.max())
+}
+
+private fun List<String>.toCalories(): List<Int> {
+    val calories = mutableListOf<Int>()
+    var count = 0
+    for (line in this) {
+        if (line.isNotBlank()) {
+            count += line.toInt()
+        } else {
+            calories.add(count)
+            count = 0
+        }
+    }
+
+    return calories
 }
